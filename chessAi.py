2426a3b2229aa15e3660b29bef5841085ab77ae1
Nick <nb12hz@brocku.ca>
2016-12-22 16:38:05 -0500

    Piece rules up to Queen

--- chessAi.py
+++ chessAi.py
@@ -17,7 +17,8 @@ board
 def intializeBoard():
     global board
     board = ['r','n','b','q','k','b','n','r'],['p1','p2','p3','p4','p5','p6','p7','p8'],['','','','','','','',''],['','','','','','','',''],['','','','','','','',''],['','','','','','','',''],['P1','P2','P3','P4','P5','P6','P7','P8'],['R','N','B','Q','K','B','N','R']
-    
+
+#Move the piece in the given coordinates to the target coordinates  
 def movePiece(startX, startY, endX, endY):
     global board    
     if (board[startY][startX]!=''):
@@ -39,41 +40,173 @@ def isValidMove(startX, startY, endX, endY):
     global board
     valid = True
     
+    #Get current and target piece type
     piece = (board[startY][startX])[0]
-    print(piece)
+    if board[endY][endX] != '':
+        targetPiece = (board[endY][endX])[0]
+    else:
+        targetPiece = ''
+    
+    #No piece to move
     if piece=='':
         valid = False
+       
+    #If the piece is friendly
+    elif targetPiece!='' and piece.isupper() and targetPiece.isupper():
+        valid=False
+        
+    #If the piece is friendly   
+    elif targetPiece!='' and piece.islower() and targetPiece.islower():
+        valid=False
+    
+    #Move the king
     elif piece=='k'or piece=='K':
         if abs(endX-startX)<=1 or abs(endY-startY)<=1:
-            return valid
-        
+            if (board[startY][startX]==''):
+                return valid
+            else:
+                valid = False
+                return valid
+    
+    #Move the Rook
     elif piece=='r' or piece=='R':
-        if (startX==endX and startY!=endY) or (startY==endY and startX!=endX):
-            return valid
+        #make sure it moves in a line
+        if (startX==endX and startY!=endY):
+            if endY>startY:
+                for i in range(startY+1,endY):
+                    if board[i][startX]!='':
+                        valid = False
+                return valid
+            else:
+                for i in range(startY-1,endY,-1):
+                    if board[i][startX]!='':
+                        valid = False
+                return valid
+        #make sure it moves in a line
+        elif (startY==endY and startX!=endX):
+            if endX>startX:
+                for i in range(startX+1,endX):
+                    if board[startY][i]!='':
+                        valid = False
+                return valid
+            else:
+                for i in range(startX-1,endX,-1):
+                    if board[startY][i]!='':
+                        valid = False
+                return valid
         else:
             valid=False
-            
+    
+    #Move the knight
     elif piece=='n'or piece=='N':
         if (abs(startX-endX)==2 and abs(startY-endY)==1) or (abs(startY-endY)==2 and abs(startX-endX)==1):
             return valid
         else:
             valid = False
-            
+    
+    #Move the bishop
     elif piece=='b'or piece=='B':
+        #Check that the move is diagonal
         if (abs(startX-endX)==abs(startY-endY) and startX!=endX and startY!=endY):
+            difference = abs(startX-endX)-1
+            
+            #Left and up
+            if startX>endX and startY>endY:
+                for i in range(difference):
+                    if board[startY-1-i][startX-1-i]!='':
+                        valid=False
+                        return valid
+                        
+            #Left and down
+            elif startX>endX and startY<endY:
+                for i in range(difference):
+                    if board[startY+1+i][startX-1-i]!='':
+                        valid=False
+                        return valid
+                        
+            #Right and Up
+            elif startX<endX and startY>endY:
+                for i in range(difference):
+                    if board[startY-1-i][startX+1+i]!='':
+                        valid=False
+                        return valid
+                        
+            #Right and Down
+            elif startX<endX and startY<endY:
+                for i in range(difference):
+                    if board[startY+1+i][startX+1+i]!='':
+                        valid=False
+                        return valid
+                        
             return valid
         else:
             valid = False
-            
+    
+    #Move the Queen    
     elif piece=='q'or piece=='Q':
-        #moving like a bishop
+        #moving like a Bishop
         if (abs(startX-endX)==abs(startY-endY) and startX!=endX and startY!=endY):
+            difference = abs(startX-endX)-1
+            
+            #Left and up
+            if startX>endX and startY>endY:
+                for i in range(difference):
+                    if board[startY-1-i][startX-1-i]!='':
+                        valid=False
+                        return valid
+                        
+            #Left and down
+            elif startX>endX and startY<endY:
+                for i in range(difference):
+                    if board[startY+1+i][startX-1-i]!='':
+                        valid=False
+                        return valid
+                        
+            #Right and Up
+            elif startX<endX and startY>endY:
+                for i in range(difference):
+                    if board[startY-1-i][startX+1+i]!='':
+                        valid=False
+                        return valid
+                        
+            #Right and Down
+            elif startX<endX and startY<endY:
+                for i in range(difference):
+                    if board[startY+1+i][startX+1+i]!='':
+                        valid=False
+                        return valid
+                        
             return valid
+        #Moving like a Rook
         elif (startX==endX and startY!=endY) or (startY==endY and startX!=endX):
-            return valid
+            #make sure it moves in a line
+            if (startX==endX and startY!=endY):
+                if endY>startY:
+                    for i in range(startY+1,endY):
+                        if board[i][startX]!='':
+                            valid = False
+                    return valid
+                else:
+                    for i in range(startY-1,endY,-1):
+                        if board[i][startX]!='':
+                            valid = False
+                    return valid
+            #make sure it moves in a line
+            elif (startY==endY and startX!=endX):
+                if endX>startX:
+                    for i in range(startX+1,endX):
+                        if board[startY][i]!='':
+                            valid = False
+                    return valid
+                else:
+                    for i in range(startX-1,endX,-1):
+                        if board[startY][i]!='':
+                            valid = False
+                    return valid
         else:
             valid = False
-            
+    
+    #Move pawns
     """elif piece=='p'or piece=='P':
         if peice=='p':
             if
