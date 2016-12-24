9576b161d800905521dc1eb2dd4f7513722a9f44
mb14sn <mb14sn@brocku.ca>
2016-12-24 12:27:49 -0500

    Minimax: Integer -> sys, chessAI: started invalid move messages

--- minimax.py
+++ minimax.py
@@ -6,7 +6,7 @@ Created on Sat Dec 24 10:41:13 2016
 """
 
 import copy
-import Integer
+import sys
     
 class minimax:
     def __init__(self, board, pawnMoved, movedTwo, whiteKS, whiteQS, blackKS, blackQS, whiteKing, blackKing):
@@ -35,7 +35,7 @@ class minimax:
         if self.isCheckmate():
             return self.evaluateGame()
             
-        maxScore = Integer.MIN_VALUE
+        maxScore = -sys.maxint -1
         
         return maxScore
         
@@ -45,7 +45,7 @@ class minimax:
         if self.isCheckmate():
             return self.evaluateGame()
             
-        minScore = Integer.MAX_VALUE
+        minScore = sys.maxint
         
         
         return minScore
