8f2980895ba5647d0d43eef6acfaad771a552b1c
mb14sn <mb14sn@brocku.ca>
2017-01-05 14:43:06 -0500

    Basic board and button layout, board needs some adjustments and movements need implemented

--- ChessGUI.py
+++ ChessGUI.py
@@ -12,47 +12,46 @@ class ChessFrame(wx.Frame):
     def __init__(self, parent):
         wx.Frame.__init__(self, parent)
         
-        #Master Panel
-        self.masterPanel = wx.Panel(self)
-        
         #Setup board with Black and White coloured squares
-        self.boardGrid = wx.grid.Grid(self, self.masterPanel)
+        self.boardGrid = wx.grid.Grid(self, -1)
         self.boardGrid.CreateGrid(8,8)
-        self.boardGrid.SetColSizes(self, 20)
-        self.boardGrid.SetRowSizes(self, 20)
+        self.boardGrid.SetCellBackgroundColour(0, 0, 'BLACK')
+        self.boardGrid.SetDefaultColSize(50, True)
+        self.boardGrid.SetDefaultRowSize(50, True)
         for y in range(8):
             for x in range(8):
                 if ((y%2)+x)%2==0:
-                    self.boardGrid.SetCellBackgroundColour(self, y, x, wx.WHITE)
+                    self.boardGrid.SetCellBackgroundColour(y, x, 'WHITE')
                 else:
-                    self.boardGrid.SetCellBackgroundColour(self, y, x, wx.BLACK)
+                    self.boardGrid.SetCellBackgroundColour(y, x, 'BLACK')
                     
         #Setup the control panel, which has an input box, last move output, and button
-        self.controls = wx.Panel(self, self.masterPanel)
+        self.controls = wx.Panel(self)
         self.button = wx.Button(self.controls, label="Move piece")
         self.nextMove = wx.StaticText(self.controls, label="Your move:")
         self.moveInput = wx.TextCtrl(self.controls, size=(100, -1))
         self.lastMove = wx.StaticText(self.controls, label="Last move made:")
-        self.moveOutput = wx.staticText(self.controls, label="")
+        self.moveOutput = wx.StaticText(self.controls, label="")
         
         #Frame sizer
         self.frameSizer = wx.BoxSizer()
-        self.frameSizer.add(self.masterPanel, 1)
+        self.frameSizer.Add(self.controls, 1, wx.ALL | wx.EXPAND)
         
         #Content holding/arranging sizer
-        self.contentSizer = wx.GridBagSizer(3, 6)
+        self.contentSizer = wx.GridBagSizer(5, 5)
         self.contentSizer.Add(self.boardGrid, (0, 0))
-        self.contentSizer.Add(self.nextMove, (1, 0))
-        self.contentSizer.Add(self.moveInput, (1, 1))
-        self.contentSizer.Add(self.button, (1,2))
-        self.contentSizer.Add(self.lastMove, (1, 3))
-        self.contentSizer.Add(self.moveOutput, (1, 4))
+        self.contentSizer.Add(self.nextMove, (1, 1))
+        self.contentSizer.Add(self.moveInput, (1, 2))
+        self.contentSizer.Add(self.button, (2, 1), (2, 2), flag=wx.EXPAND)
+        self.contentSizer.Add(self.lastMove, (3, 1))
+        self.contentSizer.Add(self.moveOutput, (3, 2))
         
         #Implement the sizers
-        self.SetSizerAndFit(self.masterPanel)
+        self.controls.SetSizerAndFit(self.contentSizer)
+        self.SetSizerAndFit(self.frameSizer)
         
         #Event handler
-        self.button.Bind(wx.EVT_Button, self.ButtonPress)
+        self.button.Bind(wx.EVT_BUTTON, self.ButtonPress)
         
     def ButtonPress(self, e):
         files = ['A','B','C','D','E','F','G','H']
@@ -68,4 +67,3 @@ app = wx.App(False)
 frame = ChessFrame(None)
 frame.Show()
 app.MainLoop()
-        
